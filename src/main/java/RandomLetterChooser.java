9626e56f4c4d06fbcbb4114b45362855c499e1e1
chyeung1 <143123817+chyeung1@users.noreply.github.com>
2024-04-21 00:42:55 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -4,7 +4,9 @@ public class RandomLetterChooser extends RandomStringChooser
 	public RandomLetterChooser(String str)
 	{ 
 		/* to be implemented in part (b) */
-		
+		for(int i = 0; i < str.length() - 1; i++){
+			array.add(str.substring(i, i + 1);
+		}
 	}
 	
 	
